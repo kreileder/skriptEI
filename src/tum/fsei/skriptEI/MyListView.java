d7b4af9467656476e4e0059eb075d52b7599dc48
Matthias Kreileder <matthias@matthias-K52JU.(none)>
2014-03-30 09:59:20 +0200

    Changed ListView to simpler version

--- src/tum/fsei/skriptEI/MyListView.java
+++ src/tum/fsei/skriptEI/MyListView.java
@@ -3,6 +3,7 @@ package tum.fsei.skriptEI;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -45,19 +46,19 @@ public class MyListView extends ListActivity {
         lstView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         lstView.setTextFilterEnabled(true);
         
-        //setListAdapter(new ArrayAdapter<String>(this, 
-        //    android.R.layout.simple_list_item_checked, subjects));
+        setListAdapter(new ArrayAdapter<String>(this, 
+        android.R.layout.simple_list_item_checked, subjects));
         
-        CustomArrayAdapter adapter = new CustomArrayAdapter(this, subjects, imageIDs);
-        setListAdapter(adapter);
+       // CustomArrayAdapter adapter = new CustomArrayAdapter(this, subjects, imageIDs);
+       // setListAdapter(adapter);
         
     }
 
-//    public void onListItemClick(ListView parent, View v, 
-//    int position, long id) {
-//        CheckedTextView item = (CheckedTextView) v;
-//        Toast.makeText(this, subjects[position] + " ausgewaehlt : " + item.isChecked(), 
-//            Toast.LENGTH_SHORT).show();
-//    }
+    public void onListItemClick(ListView parent, View v, 
+    int position, long id) {
+        CheckedTextView item = (CheckedTextView) v;
+        Toast.makeText(this, subjects[position] + " ausgewaehlt : " + item.isChecked(), 
+            Toast.LENGTH_SHORT).show();
+    }
     
 }
